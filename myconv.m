08ec2968a2be5e0b8ba165c7f54cab4f1245568d
Kubilayatas <kubilay1233@gmail.com>
2022-11-03 10:02:56 +0300

    Update myconv.m

--- myconv.m
+++ myconv.m
@@ -13,33 +13,34 @@ disp(strcat('[Mx1,Nx1]: [',num2str(Mx1),',',num2str(Nx1),']'))
 disp(strcat('[Mx2,Nx2]: [',num2str(Mx2),',',num2str(Nx2),']'))
 disp(strcat('[My,Ny]: [',num2str(My),',',num2str(Ny),']'))
 yn = zeros(1,Ny-My+1);
-for i=[1:(length(x1n)+length(x2n)-1)]
-    Multipl = x1n.*x2n';
-    diagon = i-length(x2n);
-    yn(i) = sum(diag(flipud(Multipl),diagon));
+Multipl = x1n'*x2n;
+Multipl = flipud(Multipl);
+for j=[1:(length(x1n)+length(x2n)-1)]
+    diagon = j-length(x1n);
+    yn(j) = sum(diag(Multipl,diagon));
 end
 figure;
 
 subplot(2,2,1);
 stem([Mx1:Nx1],x1n,'filled');
 title('x1[n] signal');
-xlim('tight');
+%xlim('tight');
 grid on
 
 subplot(2,2,2);;
 stem([Mx2:Nx2],x2n,'filled');
 title('x2[n] signal');
-xlim('tight');
+%xlim('tight');
 grid on
 
 subplot(2,2,3);
 stem([My:Ny],yn,'filled');
 title('y[n]=x1[n]*x2[n] signal (My Conv)');
-xlim('tight');
+%xlim('tight');
 grid on
 
 subplot(2,2,4);
 stem([My:Ny],conv(x1n,x2n),'filled');
 title('y[n]=x1[n]*x2[n] signal (Matlab Conv)');
-xlim('tight');
+%xlim('tight');
 grid on
